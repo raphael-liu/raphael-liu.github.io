baca026ab5a953c707c1f9219aa5d40fa4445f3e
liurenfei <liurenfei@baidu.com>
2016-10-25 21:10:58 +0800

    change theme

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
-echo on
+git rm -rf ./
 cd ../myhexo
 hexo clean
 hexo g
+hexo p
 hexo d
 cp -rf public/* ../raphael-liu.github.io/
+cp -f CNAME ../raphael-liu.github.io/
+cp -f run.sh ../raphael-liu.github.io/
 cd ../raphael-liu.github.io
+chmod +x run.sh
