67e3626f68fae968d3b292564cb723806131650c
liurenfei <liurenfei@baidu.com>
2016-11-17 15:02:20 +0800

    update blog

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cd ../myhexo
 hexo clean
 hexo g
 hexo d
-cp -rf static/ ../raphael/liu.github.io/static/
+cp -rf static/ ../raphael-liu.github.io/static/
 cp -rf public/* ../raphael-liu.github.io/
 cp -f CNAME ../raphael-liu.github.io/
 cp -f 404.html ../raphael-liu.github.io/
