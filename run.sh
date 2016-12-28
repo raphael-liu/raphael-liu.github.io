83cb20bcb1839415963a09c7a2842db276a16514
liurenfei <liurenfei@baidu.com>
2016-12-28 18:11:34 +0800

    update files

--- run.sh
+++ run.sh
@@ -8,6 +8,5 @@ cp -rf public/* ../raphael-liu.github.io/
 cp -f CNAME ../raphael-liu.github.io/
 cp -f 404.html ../raphael-liu.github.io/
 cp -f run.sh ../raphael-liu.github.io/
-cp -f atom.xml ../raphael-liu.github.io/
 cd ../raphael-liu.github.io
 chmod +x run.sh
