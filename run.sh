4bee3d4242af8c9efb9b33f2071456d9d3365654
raphael-liu <raphael.liu@qq.com>
2017-08-31 17:21:21 +0800

    base version of liurf.com

--- run.sh
+++ run.sh
@@ -5,8 +5,6 @@ hexo g
 hexo d
 cp -rf static/ ../raphael-liu.github.io/static/
 cp -rf public/* ../raphael-liu.github.io/
-cp -f CNAME ../raphael-liu.github.io/
-cp -f 404.html ../raphael-liu.github.io/
-cp -f run.sh ../raphael-liu.github.io/
+cp -rf copy/ ../raphael-liu.github.io/
 cd ../raphael-liu.github.io
 chmod +x run.sh
