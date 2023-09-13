98314029d0b9b7ddc492e29674062f76ae1f52ef
raphael-liu <raphael.liu@qq.com>
2023-09-13 17:08:15 +0800

    变更主题”

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-git rm -rf ./
+#git rm -rf ./
 cd ../hexo-raphael
 hexo clean
 hexo g
 hexo d
-cp -rf static/ ../raphael-liu.github.io/static/
+#cp -rf static/ ../raphael-liu.github.io/static/
 cp -rf public/* ../raphael-liu.github.io/
 cp -rf copy/ ../raphael-liu.github.io/
 cd ../raphael-liu.github.io
